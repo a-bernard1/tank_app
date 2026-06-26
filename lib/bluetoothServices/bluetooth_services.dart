d4201fff8adb2a386d98a50b9e6b42768f08b2b9
Antoine <antoine.bnrd2@gmail.com>
2026-06-26 16:14:25 +0200

    fix(bluetooth): robustify permissions and secure scan results stream

--- lib/bluetoothServices/bluetooth_services.dart
+++ lib/bluetoothServices/bluetooth_services.dart
@@ -9,6 +9,8 @@ class BluetoothManager {
   BluetoothDevice? _device;
   BluetoothCharacteristic? _characteristic;
 
+  bool _isScanning = false;
+
   final String targetDeviceName = "MOUHAHA";
   final Guid serviceUuid = Guid("19B10000-E8F2-537E-4F6C-D104768A1214");
   final Guid characteristicUuid = Guid("19B10001-E8F2-537E-4F6C-D104768A1214");
@@ -18,20 +20,32 @@ class BluetoothManager {
   }
 
   Future<bool> requestBluetoothPermissions() async {
-    final status = await [
+    Map<Permission, PermissionStatus> statuses = await [
       Permission.bluetoothScan,
       Permission.bluetoothConnect,
       Permission.location
     ].request();
-    return status.values.every((s) => s.isGranted);
+
+    return statuses.values.every((status) => status.isGranted);
   }
 
   void _startScan() async {
-    var status = await requestBluetoothPermissions();
+    if (_isScanning) return;
 
+    print(" ========== trying to ask permissions =============================");
+    var status = await requestBluetoothPermissions();
     if (!status) {
-      print("perm not allowed");
+      print(" ========== perm not allowed =============================");
+      return;
     }
+
+
+    var locationStatus = await Permission.locationWhenInUse.serviceStatus;
+    if (locationStatus.isDisabled) {
+      print("Le GPS est désactivé ! Veuillez activer la localisation dans les paramètres d'Android");
+      return;
+    }
+
 
     bool isOn = await FlutterBluePlus.isOn;
     if (!isOn) {
@@ -39,38 +53,60 @@ class BluetoothManager {
       return;
     }
 
-    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5));
+    _isScanning = true;
+    print("========== Starting Scan ==========");
 
-    FlutterBluePlus.scanResults.listen((results) async {
-      for (ScanResult r in results) {
-        print("result : ${r.device.name}");
-        if (r.advertisementData.advName == targetDeviceName) {
-          print("found device: ${r.advertisementData.advName}");
-          FlutterBluePlus.stopScan();
-          await _connectToDevice(r.device);
-          break;
-        } else {
-          print("fail !!!");
+    try {
+      await FlutterBluePlus.startScan(timeout: const Duration(seconds: 15));
+      FlutterBluePlus.scanResults.listen((results) async {
+        for (ScanResult r in results) {
+          String deviceName = r.advertisementData.advName.isNotEmpty
+              ? r.advertisementData.advName
+              : r.device.platformName;
+          print("result : ${r.device}");
+
+
+          if (deviceName == targetDeviceName && _device == null) {
+            print("Found device: $deviceName. Stopping scan and connecting...");
+            _isScanning = false;
+            await FlutterBluePlus.stopScan();
+            await _connectToDevice(r.device);
+            break;
+          }
         }
-      }
-    });
+      });
+
+
+    } catch (e) {
+      print("Erreur scan: $e");
+      _isScanning = false;
+    }
+
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 15));
   }
 
   Future<void> _connectToDevice(BluetoothDevice device) async {
-    await device.connect();
-    _device = device;
+    try {
+      await device.connect(autoConnect: false);
+      _device = device;
+      print("Connected to ${device.platformName}");
 
-    List<BluetoothService> services = await device.discoverServices();
-    for (var service in services) {
-      if (service.uuid == serviceUuid) {
-        for (var characteristic in service.characteristics) {
-          if (characteristic.uuid == characteristicUuid) {
-            _characteristic = characteristic;
-            print("found !!!!!");
-            return;
+      List<BluetoothService> services = await device.discoverServices();
+      for (var service in services) {
+        if (service.uuid == serviceUuid) {
+          for (var characteristic in service.characteristics) {
+            if (characteristic.uuid == characteristicUuid) {
+              _characteristic = characteristic;
+              print("Characteristic found and ready !");
+              return;
+            }
           }
         }
       }
+    } catch (e) {
+      print("Error during connection: $e");
+      _device = null;
+      _isScanning = false;
     }
   }
 
